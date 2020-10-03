9b80a2d131eb8b3e7699ddd64893d843df3fa7d4
Lumenmon <lumenmon.tien@namtech.sg>
2020-10-03 19:24:43 +0700

    some little edit

--- src/HomePage.js
+++ src/HomePage.js
@@ -13,7 +13,7 @@ function HomePage(props) {
   if (match.path === "/") dispatch(unVisible());
 
   return (
-    <div className="app" style={{ paddingBottom: `3rem` }}>
+    <div className="app" style={{ paddingBottom: `2.5rem` }}>
       <Banner />
       <Row
         title="NETFLIX ORIGINAL"
